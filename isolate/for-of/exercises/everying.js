428293330318e9fb95689301835c68e8127d13c9
Olga <okozmovskaya@icloud.com>
2020-07-07 22:23:08 +0200

    Completed everying

--- isolate/for-of/exercises/everying.js
+++ isolate/for-of/exercises/everying.js
@@ -9,9 +9,10 @@ const assert = chai.assert;
 const containsNoNumbers = (arr) => {
   if (!Array.isArray(arr)) { throw new TypeError('arr'); }
 
-  let noNumbers = _;
-  for (let _ of _) {
-
+  let noNumbers = true;
+  for (let item of arr) {
+    noNumbers = noNumbers && (typeof item !== 'number')
+     
   }
 
   return noNumbers;
